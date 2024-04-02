735d5165b5908ba1e345446f58c9bff806a58083
xZhou <xinzhou0417@gmail.com>
2024-04-03 04:31:59 +1100

    Modified Response class, fit the requirements of AWS API: <https://repost.aws/knowledge-center/malformed-502-api-gateway>

--- src/main/java/team/gpt2p5/rest/Response.java
+++ src/main/java/team/gpt2p5/rest/Response.java
@@ -1,31 +1,34 @@
 package team.gpt2p5.rest;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
 import java.util.HashMap;
 import java.util.List;
 
 public class Response extends HashMap<String, Object> {
 
-    private static String CODE = "code";
+    private static String CODE = "statusCode";
+    private static String BODY = "body";
     private static String MSG = "msg";
     private static String DATA = "data";
     private static String ROWS = "rows";
-//    private static String HEADERS = "headers";
 
-    private Response(int code, String msg, Object data, List<?> rows) {
-//        HashMap<String, String> headers = new HashMap<>();
-//        headers.put("Content-Type", "application/json");
-        super.put(CODE, code);
-        super.put(MSG, msg);
-        super.put(DATA, data);
-        super.put(ROWS, rows);
-//        super.put(HEADERS, headers);
+    private Response(int statusCode, String msg, Object data, List<?> rows) throws JsonProcessingException {
+        HashMap<String, Object> body = new HashMap<>();
+        body.put(MSG, msg);
+        body.put(DATA, data);
+        body.put(ROWS, rows);
+        super.put(BODY, new ObjectMapper().writeValueAsString(body));
+        super.put(CODE, statusCode);
     }
 
-    public static Response success(String msg, Object data, List<?> rows) {
+
+    public static Response success(String msg, Object data, List<?> rows) throws JsonProcessingException {
         return new Response(HttpStatusCode.SUCCESS, msg, data, rows);
     }
 
-    public static Response error(String msg, Object data, List<?> rows) {
+    public static Response error(String msg, Object data, List<?> rows) throws JsonProcessingException {
         return new Response(HttpStatusCode.ERROR, msg, data, rows);
     }
 
